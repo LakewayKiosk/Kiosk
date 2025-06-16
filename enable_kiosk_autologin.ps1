2134cf7391196b5c5a992cffadaf14f3f636967c
LakewayKiosk <leonardmolina@lakeway-tx.gov>
2025-06-16 15:32:37 -0500

    Update enable_kiosk_autologin.ps1

--- enable_kiosk_autologin.ps1
+++ enable_kiosk_autologin.ps1
@@ -1,12 +1,7 @@
 $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
-$DefaultUsername = "KIOSK_GUEST"
-$DefaultPassword = "LKWYK10sk$!"
-$DefaultDomain = "WORKGROUP"
-
-Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -Type String
-Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -Type String
-Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -Type String
-Set-ItemProperty $RegPath "DefaultDomainName" -Value "$DefaultDomain" -Type String
-
-Start-Sleep -Seconds 5
-Restart-Computer -Force
+$DefaultUsername = $args[0]
+$DefaultPassword =  $args[1] 
+Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String 
+Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String 
+Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
+Start-Sleep -Seconds 5 ; Restart-Computer -Force
